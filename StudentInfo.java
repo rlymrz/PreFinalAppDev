8ecf7e08990c582061b00e1d052a2d4eee3feacd
rlymrz <arlykacaparas22@gmail.com>
2022-01-08 15:18:50 +0800

    StudentInfo

--- StudentInfo.java
+++ StudentInfo.java
@@ -1,6 +1,7 @@
 class Student {
     public static void main (String[] args) {
         System.out.println("Arlyka Mariz Caparas");
+        System.out.println("PreFinalExam");
         System.out.println("BSIT502");
         System.out.println("App Dev");
     }
